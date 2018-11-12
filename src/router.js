ad84e6c6771d6a949f072fb4a573a41654fdef85
hanyang <shuerhy@163.com>
2018-11-12 18:53:35 +0800

    done

--- src/router.js
+++ src/router.js
@@ -4,12 +4,15 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+export const router = new Router({
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        title: '韩杨--前端开发--求职、实习'
+      }
     },
     {
       path: '/resume',
@@ -17,12 +20,25 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Resume.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/Resume.vue'),
+      meta: {
+        title: '韩杨--个人简历'
+      }
     },
     {
       path: '/danmu',
       name: 'danmu',
-      component: () => import('./components/DanMu.vue')
+      component: () => import('./components/DanMu.vue'),
+      meta: {
+        title: '弹幕是怎么练成的???'
+      }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+  next();
 })
